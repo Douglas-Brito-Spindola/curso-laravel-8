4f49349f3a7a2cc9b332f8d71856aa67e2e40225
Douglas Spíndola <douglas.spindola15@gmail.com>
2021-04-11 19:21:21 -0300

    Adicionado value mais old para pegar da sessão o valor que não foi salvo no banco e recuperar da sessão caso dei erro ao enviar o arquivo

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,7 +1,18 @@
 <h1>Novo post</h1>
+
+@if($errors->any())
+	<ul>
+		@foreach($errors->all() as $error)
+
+			<li>{{ $error }}</li>
+
+		@endforeach
+	</ul>
+@endif
+
 <form action="{{ route('posts.store')}}" method="post">
 	@csrf
-	<input type="text" name="title" placeholder="Título">
-	<textarea name="content" id="content" cols="30" rows="4" ></textarea>
+	<input type="text" name="title" placeholder="Título" value="{{ old('title')}}">
+	<textarea name="content" id="content" cols="30" rows="4">{{ old('content')}}</textarea>
 	<button type="submit">enviar</button>
 </form>
