5aa7064abe08b18f2d21900a43e4e15591658978
Douglas Spíndola <douglas.spindola15@gmail.com>
2021-04-21 11:29:38 -0300

    Criado form para deletar um registro

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -8,3 +8,9 @@
 		<strong>Conteúdo:</strong> {{$post->content}}
 	</li>
 </ul>
+
+<form action="{{ route('posts.destroy', $post->id)}}" method="post">
+	@csrf
+	<input type="hidden"  name="_method" value="delete">
+	<button type="submit">Delete o post {{$post->title}}</button>
+</form>
